82583d7914161f6c5c4bc4a750b3619914bad440
aLANparty <aLANparty@protonmail.com>
2022-02-25 13:01:29 -0500

    fix endpoint names

--- app/__init__.py
+++ app/__init__.py
@@ -15,25 +15,25 @@ def index_endpoint():
 
 # Bind CORS Preflight
 @app.route(RELAY_BASE_ROUTE + '/bind', methods=['OPTIONS'])
-def register_endpoint_options():
+def bind_endpoint_options():
     app.logger.info('bind_endpoint_options')
 
     return cors_preflight_response()
 
 # Mint CORS Preflight
 @app.route(RELAY_BASE_ROUTE + '/mint', methods=['OPTIONS'])
-def register_endpoint_options():
+def mint_endpoint_options():
     app.logger.info('mint_endpoint_options')
 
     return cors_preflight_response()
 
 # Bind
 @app.route(RELAY_BASE_ROUTE + '/bind', methods=['POST'])
-def register_endpoint():
+def bind_endpoint():
     app.logger.info('bind_endpoint')
 
     # Check to make sure a wallet address is specified.
-    addr = request.json and request.json.get('addr', '').lower()
+    addr = request.json and request.json.get('addr', '')
     if not addr:
         return cors_response(jsonify({'success': False, 'errorMessage': 'Missing address'})), 400
 
@@ -62,7 +62,7 @@ def mint_endpoint():
     app.logger.info('mint_endpoint')
 
     # Check to make sure a wallet address is specified.
-    addr = request.json and request.json.get('addr', '').lower()
+    addr = request.json and request.json.get('addr', '')
     if not addr:
         return cors_response(jsonify({'success': False, 'errorMessage': 'Missing address'})), 400
 
@@ -83,8 +83,12 @@ def mint_endpoint():
 def test_bind_endpoint():
     app.logger.info('test_bind_endpoint')
 
+    # http://localhost:5001/brightid-nft-mint-relay/test-bind?addr=0xe031628c95Df01073E95b411388deB48f09F33AA&uuidHash=0x29defd00aa3991ab2a664af0644e7ad202ed3d347f1d02703bf87ed2157145a6&nonce=4937044&signature=0x014566e7ba27a28803a5e302203cfe11d3687ca899bc96a509c43e01d60b949503bd3215a4de002ac381da75e8f854213a8eb667aaf4fe2b4852386991b446af1b
+
+    # http://localhost:5001/brightid-nft-mint-relay/test-bind?addr=0xe031628c95Df01073E95b411388deB48f09F33AA&uuidHash=0xc61618b7e38a233f1fcd244bf6504bfd5215377040437a52aeb740943c83c241&nonce=8670684&signature=0xb02f2cca5805241c280b2bc1d2ec9e2122d8839fbb58f2c8029c910cabb33658367fc83eda1a10e424f8546ac83eb8cd0fa499acecf03d7a4aa7564300d180e21b
+
     # Check to make sure a wallet address is specified.
-    addr = request.args.get('addr', '').lower()
+    addr = request.args.get('addr', '')
     if not addr:
         return jsonify({'success': False, 'errorMessage': 'Missing address'}), 400
 
@@ -113,7 +117,7 @@ def test_mint_endpoint():
     app.logger.info('test_mint_endpoint')
 
     # Check to make sure a wallet address is specified.
-    addr = request.args.get('addr', '').lower()
+    addr = request.args.get('addr', '')
     if not addr:
         return jsonify({'success': False, 'errorMessage': 'Missing address'}), 400
 
